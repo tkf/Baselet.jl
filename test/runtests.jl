90ff390613a368e8e0601f4a6094ccf31417ca4a
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-21 20:36:08 -0800

    Skip inference test on Julia 1.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,12 @@ using Test
 @testset "$file" for file in sort([
     file for file in readdir(@__DIR__) if match(r"^test_.*\.jl$", file) !== nothing
 ])
+    # Skip inference test on Julia 1.0.  It may work in 1.1 or 1.2 but
+    # they are note tested.
+    if VERSION < v"1.3-" && file == "test_inference.jl"
+        @info "Skip $file for Julia $VERSION"
+        continue
+    end
     include(file)
 end
 
